60f0b314fe664ceb5a7a28cd64339c511a9b95c2
Egon Willighagen <egon.willighagen@gmail.com>
2017-01-01 13:56:44 +0100

    Cleaned up code, preparing for further tests.

--- validate.groovy
+++ validate.groovy
@@ -18,40 +18,45 @@ if (bioclipse.isOnline()) {
   )
 }
 
+def renewFile(file) {
+  if (ui.fileExists(file)) ui.remove(file)
+  ui.newFile(file)
+  return file
+}
+
+badSMILESFile = "/Wikidata/badSMILES.txt"
+renewFile(badSMILESFile)
+smilesComboFile = "/Wikidata/wrongCombo.txt"
+renewFile(smilesComboFile)
+
 println "Found ${results.rowCount} results"
 
 for (compound=1; compound<=results.rowCount; compound++) {
   itemID = results.get(compound, "compound")
   canonical = results.get(compound, "canonical")
   isomeric = results.get(compound, "isomeric")
-  if (canonical != null && isomeric != null) {
-    if (canonical.trim().length() > 0 && isomeric.trim().length() > 0) {
-      try {
-        molCan = cdk.fromSMILES(canonical)
-        molIso = cdk.fromSMILES(isomeric)
-        canForm = cdk.molecularFormula(molCan)
-        isoForm = cdk.molecularFormula(molIso)
-        if (canForm != isoForm) {
-          ui.append("/Wikidata/wrongCombo.txt", "Canonical and isomeric SMILESes have different chemical formulas for $itemID: $canForm or $isoForm\n")
-        } 
-      } catch (Exception exception) {
-        ui.append("/Wikidata/badSMILES.txt", "Bad SMILES for $itemID: $canonical or $isomeric\n")
-      }
+  molCan = null; molIso = null
+  // check for bad SMILES
+  if (canonical != null) {
+    try {
+      molCan = cdk.fromSMILES(canonical.trim())
+    } catch (Exception exception) {
+      ui.append(badSMILESFile, "Bad canonical SMILES for $itemID: $canonical Reason: ${exception.message}\n")
     }
-  } else {
-    if (canonical != null) {
-      try {
-        molCan = cdk.fromSMILES(canonical.trim())
-      } catch (Exception exception) {
-        ui.append("/Wikidata/badSMILES.txt", "Bad canonical SMILES for $itemID: $canonical Reason: ${exception.message}\n")
-      }
+  }
+  if (isomeric != null) {
+    try {
+      molIso = cdk.fromSMILES(isomeric.trim())
+    } catch (Exception exception) {
+      ui.append(badSMILESFile, "Bad isomeric SMILES for $itemID: $isomeric Reason: ${exception.message}\n")
     }
-    if (isomeric != null) {
-      try {
-        molIso = cdk.fromSMILES(isomeric.trim())
-      } catch (Exception exception) {
-        ui.append("/Wikidata/badSMILES.txt", "Bad isomeric SMILES for $itemID: $isomeric Reason: ${exception.message}\n")
-      }
+  }
+  // compare two SMILES for expected same chemical formula
+  if (molCan != null && molIso != null) {
+    canForm = cdk.molecularFormula(molCan)
+    isoForm = cdk.molecularFormula(molIso)
+    if (canForm != isoForm) {
+      ui.append(smilesComboFile, "Canonical and isomeric SMILESes have different chemical formulas for $itemID: $canForm or $isoForm\n")
     }
   }
 }
